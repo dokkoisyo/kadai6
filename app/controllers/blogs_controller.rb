d9987396f1721d49ea2a5b330a48ac7f304cd730
sawako osada <mugiwaranana@yahoo.co.jp>
2020-08-20 07:27:23 +0000

    投稿,一覧表示機能作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,6 +3,12 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
   def create
-    Blog.create(content params[:blog][:content])
+    Blog.create(blog_params)
     redirect_to new_blog_path
+    gitnindex
+    @blogs = Blog.all
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
 end
