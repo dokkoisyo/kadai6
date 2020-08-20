f41d7f8899f71986cb51f4e57310ecf508761a89
sawako osada <mugiwaranana@yahoo.co.jp>
2020-08-20 12:51:04 +0000

    編集機能作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,8 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:edit, :update]
+  def index
+    @blogs = Blog.all
+  end
   def new
     @blog = Blog.new
   end
@@ -10,11 +14,21 @@ class BlogsController < ApplicationController
       render :new
     end
   end
-  def index
-    @blogs = Blog.all
+  def edit
   end
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ツイートを編集しました！"
+    else
+      render :edit
+    end
+  end
+
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
+  end
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_135117) do
+ActiveRecord::Schema.define(version: 2020_08_20_122756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "tweets", force: :cascade do |t|
+  create_table "blogs", force: :cascade do |t|
     t.text "content"
   end
 
